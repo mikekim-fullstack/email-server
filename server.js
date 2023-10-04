75fbc60ce0242b19a6daaf678b3d0dc8466f6868
mikekim-fullstack <mikekim.fullstack@gmail.com>
2023-10-04 14:52:18 -0400

    tried for cors2

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ contactEmail.verify((error) => {
     }
 });
 
-router.post("/contact", cors(), (req, res) => {
+router.post("/contact", (req, res) => {
     const name = req.body.firstName + req.body.lastName;
     const email = req.body.email;
     const message = req.body.message;
